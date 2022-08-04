31c0792f5290c3f5ba8e4a9a12476e568002a3de
Sumit2701 <sumitahire2701@gmail.com>
2022-08-05 01:04:54 +0530

    first commit

---
+++ weatherProject/weatherApp/views.py
@@ -0,0 +1,32 @@
+import urllib.request
+import json
+from django.shortcuts import render
+
+
+def index(request):
+
+    if request.method == 'POST':
+        city = request.POST['city']
+
+        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' +
+                                        city + '&units=metric&appid=facccd4f0dc8a0a225e164d4b8d9c9e8').read()
+        list_of_data = json.loads(source)
+
+        data = {
+            "country_code": str(list_of_data['sys']['country']),
+            "coordinate": str(list_of_data['coord']['lon']) + ', '
+            + str(list_of_data['coord']['lat']),
+            "temp": str(list_of_data['main']['temp']) + ' °C',
+            "pressure": str(list_of_data['main']['pressure']),
+            "humidity": str(list_of_data['main']['humidity']),
+            'main': str(list_of_data['weather'][0]['main']),
+            'description': str(list_of_data['weather'][0]['description']),
+            'icon': list_of_data['weather'][0]['icon'],
+            'city': city,
+            'wind': str(list_of_data['wind']['speed']),
+        }
+        print(data)
+    else:
+        data = {}
+
+    return render(request, "main/weather.html", data)
